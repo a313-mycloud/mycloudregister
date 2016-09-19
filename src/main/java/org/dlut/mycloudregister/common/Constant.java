695847dc88ca2acfc62b9d907cb79614b1bdb52a
ZhenShiErGe <895990111@qq.com>
2016-09-19 16:13:11 +0800

    更改连接地址

--- src/main/java/org/dlut/mycloudregister/common/Constant.java
+++ src/main/java/org/dlut/mycloudregister/common/Constant.java
@@ -6,5 +6,5 @@ public class Constant {
 	public final static String REGISTER_CLASS_TEACHER_ACCOUNT = "teacher";
 	
 	public final static String REGISTER_URL = "/register";
-	public final static String LOGIN_HTML="http://192.168.1.82:8080/login";
+	public final static String LOGIN_HTML="http://210.30.97.173/login";
 }
